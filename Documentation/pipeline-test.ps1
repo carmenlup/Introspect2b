244d34c3a8f74f03a145ffc6c34d6bcca528e50c
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2025-10-12 17:51:25 +0300

    check container environment creation on pipelines

--- Documentation/pipeline-test.ps1
+++ Documentation/pipeline-test.ps1
@@ -36,11 +36,40 @@ catch {
          # exit 1
       }
 
+# --- Log Analytics Workspace exists ---
+# Declare variables
+$logAnalyticsWorkspaceName = "workspace-intospect2b-logs"
+$logAnalyticsBicepFile = "log-analytics-workspace-def.bicep"
+$location = "westeurope"
+$resourceGroup = "introspect-2-b"
+Write-Host "Checking if Log Analytics Workspace exists: $logAnalyticsWorkspaceName"
+try {
+	$logAnalyticsWorkspaceNameExist= az monitor log-analytics workspace show --resource-group $resourceGroup --workspace-name $logAnalyticsWorkspaceName --query "name" --output tsv 2>$null
+	Write-Host "Value of logAnalyticsWorkspaceNameExist: $logAnalyticsWorkspaceNameExist"
+	if ($logAnalyticsWorkspaceNameExist) {
+	   Write-Host "Log Analytics Workspace $logAnalyticsWorkspaceName already exists. Deployment will not proceed."
+	   # exit 1
+	} else {
+	   Write-Host "Log Analytics Workspace $logAnalyticsWorkspaceName does not exist. Proceeding with deployment."
+	   az deployment group create `
+			--resource-group $resourceGroup `
+			--template-file $logAnalyticsBicepFile `
+			--parameters logAnalyticsWorkspaceName=$logAnalyticsWorkspaceName location=$location
+		Write-Host "Deployment completed successfully."
+	}
+
+	
+	#exit 1
+} catch {
+	Write-Host "An error occurred during the deployment process."
+	Write-Host "Error details: $($_.Exception.Message)"
+	# exit 1
+}
 
 # --- Container Environment Resource exists ---
 # Declare variables
 $containerEnvironmentName = "claimstatus-container-app-env"
-$containerEnvBicepFile = "container-env.bicep"
+$containerEnvBicepFile = "container-environment-def.bicep"
 $logAnalyticsWorkspaceName = "workspace-intospect2b-logs"
 $location = "westeurope"
 $resourceGroup = "introspect-2-b"
@@ -54,10 +83,13 @@ try {
        # exit 1
     } else {
        Write-Host "Container Environment $containerEnvironmentName does not exist. Proceeding with deployment."
+       
        az deployment group create `
             --resource-group $resourceGroup `
             --template-file $containerEnvBicepFile `
-            --parameters containerEnvironmentName=$containerEnvironmentName location=$location logAnalyticsWorkspaceName=$logAnalyticsWorkspaceName
+            --parameters containerEnvironmentName=$containerEnvironmentName `
+                location=$location 
+                logAnalyticsWorkspaceName=$logAnalyticsWorkspaceName
         Write-Host "Deployment completed successfully."
     }
 
@@ -69,3 +101,43 @@ try {
     # exit 1
 }
 
+
+
+# --- CLI commands for respources--
+# create resource group
+az group create --name "introspect-2-b" --location "westeurope"
+# create log analytics workspace
+az deployment group create `
+  --resource-group "introspect-2-b" `
+  --template-file "log-analytics-workspace-def.bicep" `
+  --parameters logAnalyticsWorkspaceName="workspace-intospect2b-logs" `
+        location="westeurope"
+
+$workspaceKey = az monitor log-analytics workspace get-shared-keys `
+  --resource-group introspect-2-b `
+  --workspace-name workspace-intospect2b-logs `
+  --query primarySharedKey `
+  --output tsv
+Write-Host "Log Analytics Workspace Key: $workspaceKey"
+
+az deployment group create `
+  --resource-group "introspect-2-b" `
+  --template-file "container-environment-def.bicep" `
+  --parameters containerEnvironmentName="claimstatus-container-app-env" 
+    location="westeurope" 
+    logAnalyticsWorkspaceId="<replace with log analytics workspace id>"
+    logAnalyticsWorkspaceKey="<replace with log analytics workspace key>"
+
+az deployment group create `
+--resource-group "introspect-2-b" `
+--template-file "aca-deploy.bicep" `
+--parameters acrName="introspect2bacr" `
+            acrLoginServer="introspect2bacr.azurecr.io" `
+            containerAppName="claim-status-app" `
+            containerImage="introspect2bacr.azurecr.io/claimstatus:latest" `
+            location="westeurope" `
+            environmentName="claimstatus-container-app-env" `
+            logAnalyticsWorkspaceName="workspace-intospect2b-logs" `
+            logAnalyticsSku="PerGB2018" `
+            cpu="0.5" `
+            memory="1.0Gi"
--- iac/container-environment-def.bicep
+++ iac/container-environment-def.bicep
@@ -4,8 +4,11 @@ param location string
 @description('The name of the Azure Container Apps environment')
 param containerEnvironmentName string // value received from command parameters
 
-@description('The name of the Azure Log Analytics workspace')
-param logAnalyticsWorkspaceName string // value received from command parameters
+@description('The Id of the Log Analytics workspace')
+param logAnalyticsWorkspaceId string // value received from command parameters
+
+@description('The key of the Log Analytics workspace')
+param logAnalyticsWorkspaceKey string // value received from command parameters
 
 resource logAnalyticsWorkspace 'Microsoft.ContainerRegistry/registries@2023-01-01-preview' existing = {
   name: logAnalyticsWorkspaceName
@@ -18,7 +21,7 @@ resource containerAppEnvironment 'Microsoft.App/managedEnvironments@2025-02-02-p
     appLogsConfiguration: {
       destination: 'log-analytics'
       logAnalyticsConfiguration: {
-        customerId: logAnalyticsWorkspace.properties.customerId
+        customerId: logAnalyticsWorkspaceId
         sharedKey: logAnalyticsWorkspace.listKeys().primarySharedKey
         dynamicJsonColumns: false
       }
